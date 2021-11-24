be239ccf20051df1b7d6e58b2edb96cefb5906e6
Patrick Leach <pl@leach.cx>
2021-11-24 12:58:41 +1100

    contact form

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -56,9 +56,7 @@ export const ContactPageTemplate = ({
           </div>
         </div>
 
-        <div>
-          <FormSimpleAjax name="Simple Form Ajax" />
-        </div>
+        
       </div>
     </section>
 
